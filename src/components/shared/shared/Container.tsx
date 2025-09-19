dc16953787f2d2c5a4c11a06612c26b265ec74d2
Evgen Kulbachenko <45nalitay45@gmail.com>
2025-09-19 22:05:54 +0300

    boders removed

--- src/components/shared/shared/Container.tsx
+++ src/components/shared/shared/Container.tsx
@@ -14,7 +14,7 @@ const Container = ({
   return (
     <Tag
       className={clsx(
-        'mx-auto border border-blue-500 md:border-green-500 lg:border-red-700 min-w-[316px] max-w-[360px] md:min-w-[768px] px-5.5 md:pl-8 md:pr-10 lg:min-w-[1920px] lg:pl-66 lg:pr-66',
+        'mx-auto min-w-[316px] max-w-[360px] md:min-w-[768px] px-5.5 md:pl-8 md:pr-10 lg:min-w-[1920px] lg:pl-66 lg:pr-66',
         additionalClass,
       )}
     >
